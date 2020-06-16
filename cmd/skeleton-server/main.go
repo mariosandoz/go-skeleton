c6d8bfc1ecc62c5d2020e785411069a573cb9eb7
Maryo Nababan <maryo@getcraft.com>
2020-06-16 22:40:01 +0700

    Add example for config

--- cmd/skeleton-server/main.go
+++ cmd/skeleton-server/main.go
@@ -11,7 +11,6 @@ import (
 func main() {
 	var adder adding.Service
 	router := rest.Handler(adder)
-	fmt.Println("The creator server is on tap now: http://localhost:3000")
+	fmt.Println("The creator server is on craft now: http://localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", router))
-
 }
